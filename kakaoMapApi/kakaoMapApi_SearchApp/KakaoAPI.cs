0df6c343e55d7d7319d6afbb79b6b3df18a244fb
SeoDongWoo1216 <dw6642@naver.com>
2021-06-03 14:07:02 +0900

    21-06-03 카카오맵API 검색 프로그램 커밋

---
+++ kakaoMapApi/kakaoMapApi_SearchApp/KakaoAPI.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+
+namespace kakaoMapApi_SearchApp
+{
+    class KakaoAPI
+    {
+        internal static List<MyLocale> Search(string query)
+        {
+            List<MyLocale> mls = new List<MyLocale>();
+            string site = "https://dapi.kakao.com/v2/local/search/keyword.json";
+            string rquery = string.Format("{0}?query={1}", site, query);
+            WebRequest request = WebRequest.Create(rquery);
+            string rkey = "d6ce3eae7e636bc49bd2c85019f768f7";
+            string header = "KakaoAK " + rkey;
+            request.Headers.Add("Authorization", header);
+
+            WebResponse response = request.GetResponse();
+            Stream stream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(stream, Encoding.UTF8);
+            String json = reader.ReadToEnd();
+
+            JavaScriptSerializer js = new JavaScriptSerializer();
+            dynamic dob = js.Deserialize<dynamic>(json);
+            dynamic docs = dob["documents"];
+            object[] buf = docs;
+            int length = buf.Length;
+            for (int i = 0; i < length; i++)
+            {
+                string lname = docs[i]["place_name"];
+                double x = double.Parse(docs[i]["x"]);
+                double y = double.Parse(docs[i]["y"]);
+                mls.Add(new MyLocale(lname, y, x));
+            }
+            return mls;
+        }
+    }
+}
---
+++ kakaoMapApi/kakaoMapApi_SearchApp/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace kakaoMapApi_SearchApp
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_search_Click(object sender, RoutedEventArgs e)
+        {
+            List<MyLocale> mls = KakaoAPI.Search(tbox_query.Text);
+            lbox_locale.ItemsSource = mls;
+        }
+
+        private void lbox_locale_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (lbox_locale.SelectedIndex == -1)
+            {
+                return;
+            }
+
+            MyLocale ml = lbox_locale.SelectedItem as MyLocale;
+            object[] ps = new object[] { ml.Lat, ml.Lng };
+            wb.InvokeScript("setCenter", ps);
+        }
+    }
+}
