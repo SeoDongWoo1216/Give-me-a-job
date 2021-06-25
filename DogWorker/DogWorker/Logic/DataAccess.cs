c95e4151ff70fe24eacfe2deec1a7e876414febb
guemin96 <guemin12@naver.copm>
2021-06-25 14:42:52 +0900

    WPF업로드

---
+++ DogWorker/DogWorker/Logic/DataAccess.cs
@@ -0,0 +1,44 @@
+﻿//using DogWorker.Model;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DogWorker.Logic
+{
+    public class DataAccess
+    {
+        //셋팅 테이블에서 데이터 가져오기
+        //public static List<Settings> GetSettings()
+        //{
+        //    List<Model.Settings> settings;
+        //    using (var ctx = new MRPEntities()) // ctx에 MRP데이터를 넣는 행동 , new MRPEntities를 사용함으로써 커넥션 커맨드 파라미터 역할을 해줌
+        //    {
+        //        settings = ctx.Settings.ToList();
+        //    }// using을 사용함으로써 데이터베이스를 열고 알아서 닫아주는 역할을 함
+        //    return settings;
+
+        //}
+
+        //public static int SetSettings(Settings item)
+        //{
+        //    using (var ctx = new MRPEntities())
+        //    {
+        //        ctx.Settings.AddOrUpdate(item);
+        //        return ctx.SaveChanges(); //COMMIT
+        //    }
+        //}
+
+        //public static int DelSetting(Settings item)
+        //{
+        //    using (var ctx = new MRPEntities())
+        //    {
+        //        var obj = ctx.Settings.Find(item.BasicCode);//DB안에서 데이터를 찾아야 삭제할 수 있음(BasicCode를 통해=>PK이기 때문에)
+        //        ctx.Settings.Remove(obj); //DB데이터를 삭제하는 과정
+        //        return ctx.SaveChanges();
+        //    }
+        //}
+    }
+}
---
+++ DogWorker/DogWorker/MainWindow.xaml.cs
@@ -0,0 +1,167 @@
+﻿using MahApps.Metro.Controls;
+using MahApps.Metro.Controls.Dialogs;
+using System;
+using System.Windows;
+
+using DogWorker.View.DogInf;
+using DogWorker.View.DogWorkerInf;
+using DogWorker.View.CourseInf;
+using DogWorker.View.Record;
+using DogWorker.View.Apply;
+
+namespace DogWorker
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : MetroWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MetroWindow_ContentRendered(object sender, EventArgs e)
+        {
+            ShowLoginView();
+        }
+
+        private void MetroWindow_Activated(object sender, EventArgs e)
+        {
+            //if (Commons.LOGINED_USER != null)
+            //    BtnLoginedId.Content = $"{Commons.LOGINED_USER.UserEmail} ({Commons.LOGINED_USER.UserName})";
+        }
+        private void ShowLoginView()
+        {
+
+        }
+
+        private async void BtnAccount_Click(object sender, RoutedEventArgs e)
+        {
+            //try
+            //{
+            //    ActiveControl.Content = new MyAccount();
+            //}
+            //catch (Exception ex)
+            //{
+            //    Commons.LOGGER.Error($"예외발생 BtnAccount_Click : {ex}");
+            //    await this.ShowMessageAsync("예외", $"예외발생 : {ex}");
+            //}
+        }
+
+        private async void BtnUser_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                //ActiveControl.Content = new UserList();
+            }
+            catch (Exception ex)
+            {
+                Commons.LOGGER.Error($"예외발생 BtnUser_Click : {ex}");
+                await this.ShowMessageAsync("예외", $"예외발생 : {ex}");
+            }
+        }
+
+        private void BtnStore_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                // ActiveControl.Content = new StoreList();
+            }
+            catch (Exception ex)
+            {
+                Commons.LOGGER.Error($"예외발생 BtnStore_Click : {ex}");
+                this.ShowMessageAsync("예외", $"예외발생 : {ex}");
+            }
+        }
+
+        private async void BtnExit_Click(object sender, RoutedEventArgs e)
+        {
+            var result = await this.ShowMessageAsync("종료", "프로그램을 종료하시겠습니까?",
+                MessageDialogStyle.AffirmativeAndNegative, null);
+            if (result == MessageDialogResult.Affirmative)
+            {
+                Application.Current.Shutdown();
+            }
+
+        }
+
+        private void BtnSetting_Click(object sender, RoutedEventArgs e)
+        {
+            //try
+            //{
+            //    ActiveControl.Content = new SettingList();
+            //}
+            //catch (Exception ex)
+            //{
+            //    Commons.LOGGER.Error($"예외발생 BtnSetting_Click : {ex}");
+            //    this.ShowMessageAsync("예외", $"예외발생 : {ex}");
+            //}
+        }
+
+        private void BtnDogInf_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                ActiveControl.Content = new DogInfMain();
+            }
+            catch (Exception ex)
+            {
+                Commons.LOGGER.Error($"예외발생 BtnDogInf_Click : {ex}");
+                this.ShowMessageAsync("예외", $"예외발생 : {ex}");
+            }
+        }
+
+        private void BtnWorkerInf_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                ActiveControl.Content = new DWIMain();
+            }
+            catch (Exception ex)
+            {
+                Commons.LOGGER.Error($"예외발생 BtnWorkerInf_Click : {ex}");
+                this.ShowMessageAsync("예외", $"예외발생 : {ex}");
+            }
+        }
+
+        private void BtnCourseInf_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                ActiveControl.Content = new CourseInfMain();
+            }
+            catch (Exception ex)
+            {
+                Commons.LOGGER.Error($"예외발생 BtnCourseInf_Click : {ex}");
+                this.ShowMessageAsync("예외", $"예외발생 : {ex}");
+            }
+        }
+
+        private void Btnrecord_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                ActiveControl.Content = new RecordMain();
+            }
+            catch (Exception ex)
+            {
+                Commons.LOGGER.Error($"예외발생 Btnrecord_Click : {ex}");
+                this.ShowMessageAsync("예외", $"예외발생 : {ex}");
+            }
+        }
+
+        private void BtnApply_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                ActiveControl.Content = new CSApplyMain();
+            }
+            catch (Exception ex)
+            {
+                Commons.LOGGER.Error($"예외발생 BtnApply_Click : {ex}");
+                this.ShowMessageAsync("예외", $"예외발생 : {ex}");
+            }
+        }
+    }
+}
