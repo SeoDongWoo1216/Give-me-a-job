4d4cbeae6604dcc204a238c1915a4f5deed06f91
hyojin-park24 <qkrgywls332@gmail.com>
2021-07-29 17:31:20 +0900

    0729 MQTT 01

--- gps/gps_mqtt.py
+++ gps/gps_mqtt.py
@@ -1,8 +1,10 @@
+from typing import OrderedDict      #데이터 순서대로 받기
 import serial
-import time 
-import string
+import string 
 import pynmea2
- 
+import paho.mqtt.client as mqtt
+import datetime as dt
+
 while True:
     port = "/dev/ttyAMA0"
     ser = serial.Serial(port, baudrate=9600, timeout=0.5)
@@ -18,4 +20,12 @@ while True:
         lat = new_gps.latitude  
         lon = new_gps.longitude  
         gps = "Latitude = " + str(lat) + " Longitude = " + str(lon)
-        print(gps)
+        # print(gps)
+
+# 그런데 안끝내고 계속 데이터를 받을 건가? 
+# 산책이 끝나서 종료 버튼을 누르면 종료 되어야할텐데 
+# 이건 여기서 하는게 아닐까? (WPF에서 하는 건가)
+
+# 데이터를 MQTT로 보내는 메서드
+def send_date(result):
+    pass 
