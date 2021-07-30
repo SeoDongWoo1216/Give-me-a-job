a24cfdb58b6b024a6a4f2db94ee7dfeb6fa5ba47
hyojin-park24 <qkrgywls332@gmail.com>
2021-07-30 17:15:54 +0900

    0730 GPS_MQTT01

--- gps/gps_mqtt.py
+++ gps/gps_mqtt.py
@@ -1,31 +1,62 @@
 from typing import OrderedDict      #데이터 순서대로 받기
 import serial
 import string 
-import pynmea2
+import pynmea2          #프로토콜을 위한 파이썬 라이브러리
 import paho.mqtt.client as mqtt
 import datetime as dt
+import uuid
+import json
 
-while True:
-    port = "/dev/ttyAMA0"
-    ser = serial.Serial(port, baudrate=9600, timeout=0.5)
-    dataout = pynmea2.NMEAStreamReader()
-    newdata = ser.readline()
-    #print(newdata) 
+# MQTT 브로커 설정 
+dev_id = 'GPS01'
+dev_uid = uuid.uuid3(uuid.NAMESPACE_OID, dev_id)
+broker_address = '192.168.200.105'
 
-    newdata_u = newdata.decode('utf-8')
-    #print(newdata_u)
-    
-    if newdata_u[0:6] == "$GPRMC":
-        new_gps = pynmea2.parse(newdata_u)
-        lat = new_gps.latitude  
-        lon = new_gps.longitude  
-        gps = "Latitude = " + str(lat) + " Longitude = " + str(lon)
-        # print(gps)
+# GPS 데이터 받아오기 
+def loop():
+    result = ""
 
-# 그런데 안끝내고 계속 데이터를 받을 건가? 
-# 산책이 끝나서 종료 버튼을 누르면 종료 되어야할텐데 
-# 이건 여기서 하는게 아닐까? (WPF에서 하는 건가)
+    while True:
+        port = "/dev/ttyAMA0"      # 우리가 사용하는 시리얼 이름 
+        ser = serial.Serial(port, baudrate=9600, timeout=0.5)
+        #(포토, 전송 속도 = 9600, 시간초과=0.5)
+        dataout = pynmea2.NMEAStreamReader()
+        #데이터 출력을 NMEA로 출력하겠음 
+        #NMEA(위치전송 규격)문장의 스트림을 데이터 처리 가능 
+        #GPGGA - 위동,경도 알려줌 
+        newdata = ser.readline()
+        newdata_u = newdata.decode('utf-8')
 
-# 데이터를 MQTT로 보내는 메서드
-def send_date(result):
-    pass 
+        if newdata_u[0:6] == "$GPRMC":
+            new_gps = pynmea2.parse(newdata_u)  #newdata_u를 파씽
+            if new_gps.latitude :
+                result = "lat"
+            elif new_gps.longitude:
+                result = "lon"
+
+            
+
+#데이터를 MQTT로 보내는 메서드
+def send_data(result):
+    send_msg = ''
+    if result == 'lat':
+        send_msg = 'latitude'
+    elif result == 'lon':
+        send_msg = 'longitude'
+    else:
+        send_msg = 'ERR'
+
+    currtime = dt.datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')
+
+# groupdata generate
+raw_data = OrderedDict()
+raw_data['']
+
+# main 
+if __name__=='__main__':
+    setup()
+    send_data('CONN')
+    try:
+        loop()
+    except KeyboardInterrupt:
+        pass
