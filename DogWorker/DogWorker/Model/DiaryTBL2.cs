3604b548f4c6428825d42b30ef7130e03afbf0a0
guemin96 <guemin12@naver.copm>
2021-08-04 12:08:19 +0900

    빠진 파일 커밋

---
+++ DogWorker/DogWorker/Model/DiaryTBL2.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DogWorker.Model
+{
+    public partial class DiaryTBL
+    {
+        public string Contents_Summary { get; set; }
+    }
+}
